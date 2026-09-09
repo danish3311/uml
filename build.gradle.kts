1de2ac9d35c728058dd9a6dfce665b0b213ed170
Danish Ali <danish3311@gmail.com>
2026-09-09 16:59:03 +0500

    Add Termux Maven repository for terminal-emulator and terminal-view dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,13 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://packages.termux.org/apt/termux-main")
+        }
+    }
+}
